47da3777d2bb5c96596d63d48b882cda7646069f
“vaishnavi <vaishnavisirsat2717@gmail.com>
2020-04-30 17:49:09 +0530

    Hello to terminal commands

--- Helloworld.java
+++ Helloworld.java
@@ -2,13 +2,8 @@ public class HelloWorld
 {
 	public static void main(String []ards)
 	{
-	while(true)
-		{
+	
 			System.out.println("Hello to Terminal commands");
-		try{
-			Thread.sleep(60000);
-			}
-			catch(Exception ex) {}
+	}
 }
-}
-}
+
